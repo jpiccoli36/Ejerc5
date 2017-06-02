97a24093c63ab94a9b1154aa51140a4196c4fedf
jose <jpiccoli36@gmail.com>
2017-06-02 18:06:17 -0300

    borrandocomentarios

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -1,34 +1,26 @@
 package Controlador;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import Persona.Persona;
 
 public class Controlador {
-
 	Scanner s = new Scanner(System.in);
 	ArrayList<Persona> pe = new ArrayList<Persona>();
 
 	public Controlador() {
-
 		pe.add(new Persona("jose", "piccoli", "123123", true));
 		pe.add(new Persona("analia", "salazar", "123321", false));
 	}
-
-	public void Alta(Persona p) {		
+	public void Alta(Persona p) {	
 		pe.add(p);
 	}
-
 	public void Baja(String dni) {
 		for (Persona per : pe) {
 			if (per.getDNI().equals(dni)) {
 				pe.remove(per);
 				break;
-			}
-		}
-	}
-
+			}		}	}
 	public void Modifica(Persona p) {
 		for (Persona per : pe) {
 			if (per.getDNI().equals(p.getDNI())) {
